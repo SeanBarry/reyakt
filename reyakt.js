ab411e0b230e3631f5bef46e8cbfd6f2c859f9b6
Sean Barry <se@nbarry.co.uk>
2017-12-06 15:25:14 +0000

    Added properties

--- reyakt.js
+++ reyakt.js
@@ -3,9 +3,19 @@ var Reyakt = {
     document.getElementById(destination).appendChild(component);
   },
 
-  makeTag(tag, content) {
+  makeTag(tag, properties, content) {
     var element = document.createElement(tag);
+
+    Object.keys(properties).forEach(function(property) {
+      if (property in element) {
+        element.setAttribute(property, properties[property]);
+      } else {
+        console.error(`[ERROR] - ${property} is not valid!`);
+      }  
+    });
+
     if (content) element.innerHTML = content;
+
     return element;
   }
 }
