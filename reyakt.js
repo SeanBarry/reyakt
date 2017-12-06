3230074f83772e0d5de8dc4669e527fc547cbfa3
Sean Barry <se@nbarry.co.uk>
2017-12-06 15:21:21 +0000

    Added makeTag function

--- reyakt.js
+++ reyakt.js
@@ -1,5 +1,11 @@
 var Reyakt = {
   render(destination, component) {
     document.getElementById(destination).appendChild(component);
+  },
+
+  makeTag(tag, content) {
+    var element = document.createElement(tag);
+    if (content) element.innerHTML = content;
+    return element;
   }
 }
